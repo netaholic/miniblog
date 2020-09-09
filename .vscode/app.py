4c9b04dca84e9a5b015f71c2aed274b235ee0fcc
netaholic <ahabach@gmail.com>
2020-09-09 19:08:08 +0300

    more update

---
+++ .vscode/app.py
@@ -0,0 +1,11 @@
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route("index")
+def index():
+    return 'wlecome to my mini blog testing page with flask'
+
+if __name__ = '__main__':
+    app.run(debug=true)    
