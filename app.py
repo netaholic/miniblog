4beb38e1e8a78699cdaf5c148e3e4553ccd78cae
netaholic <ahabach@gmail.com>
2020-09-09 20:29:19 +0300

    update

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'this is my test page with flask '
+    return 'this is my test page with flask <h1>testing page 1</h1> '
 
 if __name__ == '__main__':
     app.run(debug=True)    
